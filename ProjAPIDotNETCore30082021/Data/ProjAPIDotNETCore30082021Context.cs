c77b13b15f9a394f12874db4ab5d42bd694b8653
unknown <aannddrree>
2021-08-30 12:42:34 -0300

    v0

---
+++ ProjAPIDotNETCore30082021/Data/ProjAPIDotNETCore30082021Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjAPIDotNETCore30082021.Model;
+
+namespace ProjAPIDotNETCore30082021.Data
+{
+    public class ProjAPIDotNETCore30082021Context : DbContext
+    {
+        public ProjAPIDotNETCore30082021Context (DbContextOptions<ProjAPIDotNETCore30082021Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjAPIDotNETCore30082021.Model.Person> Person { get; set; }
+    }
+}
